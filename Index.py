6c21c0e41f271119614369b110b1773e78a08c0e
MachFrum <mpeter778@gmail.com>
2025-03-11 11:55:53 +0300

    End of class, currently testing if I can update the repo

--- Index.py
+++ Index.py
@@ -1,3 +1,9 @@
 print("Hello World")
-number = 10
-print(number)
+number = 10 # Integer
+age = 20 # Integer
+marks = 30.5 # Float
+name = "Peter" # String
+print(number, age, marks, name) # Print multiple variables  
+language = ["Python", "Java", "C++", 3, 5.7, True]  # List of different data types
+print(language)
+print(language[0], language[1], language[-6]) # Accessing elements of list from left to right positive index and from right to left negative index 
