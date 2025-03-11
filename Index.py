1fcd4823d3f8185fae6193b9c960fff075b9f34d
MachFrum <mpeter778@gmail.com>
2025-03-11 12:00:55 +0300

    Class ended and work pushed to the relevant repo

--- Index.py
+++ Index.py
@@ -6,4 +6,5 @@ name = "Peter" # String
 print(number, age, marks, name) # Print multiple variables  
 language = ["Python", "Java", "C++", 3, 5.7, True]  # List of different data types
 print(language)
-print(language[0], language[1], language[-6]) # Accessing elements of list from left to right positive index and from right to left negative index 
+print(language[0], language[1], language[-6]) # Accessing elements of list from left to right positive index and from right to left negative index.
+# To update repo, I git add . and git commit -m "Message" and git push origin master
